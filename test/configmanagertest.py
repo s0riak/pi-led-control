a175b8336aec6df0953265c6a7b5917ad5459fd8
Sebastian Kanis <code@sebastiankanis.de>
2016-10-03 17:01:31 +0200

    Added test for pathExist in configmanagertest

--- test/configmanagertest.py
+++ test/configmanagertest.py
@@ -15,15 +15,15 @@ class MockedConfigManager(ConfigurationManager):
                     "name1": {"intAttribute": 42},
                     "name2": {"floatAttribute": 3.5},
                     "name3": {"stringAttribute": "fortytwo"},
-                    "sunrise": {"firstAttribute": 73, "secondAttribute": "thirtyseven"}
+                    "name4": {"firstAttribute": 73, "secondAttribute": "thirtyseven"}
                 },
             "arrayOfDicts":
             [
-                {"pivotAttribute": "pivotValue1",  "otherAttribute": 21},
-                {"pivotAttribute": "pivotValue2",  "otherAttribute2": "12"},
-                {"pivotAttribute": "pivotValue3", "otherAttribute3": {"deepAttribute1": 1001001, "deepAttribute2": "Anagram", "deepAttribute3": None}},
-                {"pivotAttribute": "pivotValue4", "otherAttribute3": {"deepAttribute1": 110110, "deepAttribute2": "Anagram", "deepAttribute3": None}},
-                {"nonPivotAttribute": "nonPivotValue", "otherAttribute4": "justAnotherValue"}
+                {"pivotAttribute1": "pivotValue1",  "otherAttribute": 21},
+                {"pivotAttribute1": "pivotValue2",  "otherAttribute2": "12"},
+                {"pivotAttribute1": "pivotValue3", "otherAttribute3": {"deepAttribute1": 1001001, "deepAttribute2": "Anagram", "deepAttribute3": None}},
+                {"pivotAttribute1": "pivotValue4", "otherAttribute3": {"deepAttribute1": 110110, "deepAttribute2": "Anagram", "deepAttribute3": None}},
+                {"pivotAttribute2": "pivotValue5", "otherAttribute4": "justAnotherValue"}
             ]
         }
 
@@ -38,6 +38,39 @@ class ConfigurationManagerTest(unittest.TestCase):
 
     def test_emptyPathExits(self):
         self.assertTrue(self.config.pathExists(""))
+        
+    def test_firstLevelPathExits(self):
+        self.assertTrue(self.config.pathExists("dictOfDicts"))
+        self.assertTrue(self.config.pathExists("arrayOfDicts"))
+        self.assertFalse(self.config.pathExists("abc"))
+        
+    def test_secondLevelDictPathExists(self):
+        self.assertTrue(self.config.pathExists("dictOfDicts/name1"))
+        self.assertTrue(self.config.pathExists("dictOfDicts/name2"))
+        self.assertTrue(self.config.pathExists("dictOfDicts/name3"))
+        self.assertTrue(self.config.pathExists("dictOfDicts/name4"))
+        self.assertFalse(self.config.pathExists("dictOfDicts/abc"))
+        self.assertFalse(self.config.pathExists("dictOfDicts/name5"))
+        with self.assertRaises(KeyError):
+            self.config.pathExists("dictOfDicts/42")
+        
+    def test_secondLevelArrayByIndexPathExists(self):
+        self.assertTrue(self.config.pathExists("arrayOfDicts/0"))
+        self.assertTrue(self.config.pathExists("arrayOfDicts/1"))
+        self.assertTrue(self.config.pathExists("arrayOfDicts/2"))
+        self.assertTrue(self.config.pathExists("arrayOfDicts/3"))
+        self.assertTrue(self.config.pathExists("arrayOfDicts/4"))
+        self.assertFalse(self.config.pathExists("arrayOfDicts/5"))  
 
+        
+    def test_secondLevelArrayByAttributePathExists(self):
+        self.assertTrue(self.config.pathExists("arrayOfDicts/pivotAttribute1=pivotValue1"))
+        self.assertTrue(self.config.pathExists("arrayOfDicts/pivotAttribute1=pivotValue2"))
+        self.assertTrue(self.config.pathExists("arrayOfDicts/pivotAttribute1=pivotValue3"))
+        self.assertTrue(self.config.pathExists("arrayOfDicts/pivotAttribute1=pivotValue4"))
+        self.assertFalse(self.config.pathExists("arrayOfDicts/pivotAttribute1=pivotValue5"))
+        self.assertTrue(self.config.pathExists("arrayOfDicts/pivotAttribute2=pivotValue5"))
+        self.assertFalse(self.config.pathExists("arrayOfDicts/pivotAttribute2=pivotValue1"))
+        
 if __name__ == '__main__':
     unittest.main()
